9fe52fa40a6950a3a8d47b485ad5ec0f6a80c01d
Rémi Campistron <remi.campistron@fms-ea.com>
2025-10-22 14:41:49 +0200

    Ajout de la description des attributs de la classe Product dans la docstring

--- Product.py
+++ Product.py
@@ -7,6 +7,14 @@ from dataclasses import dataclass
 class Product:
     """
     Classe représentant un produit
+
+    Attributs:
+        id: identifiant unique du produit
+        name: nom du produit
+        stock: quantité disponible
+        price: prix en euros
+        unit: unité de poids
+
     """
     id: int
     name: str
