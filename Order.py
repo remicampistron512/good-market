cb495a9876de86137c673794cf900e50aa45b0ef
Rémi Campistron <remi.campistron@gmail.com>
2025-10-22 21:27:56 +0200

    Ajout d'un identifiant unique à la commande dans la classe Order

--- Order.py
+++ Order.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass, field
 from typing import List
 from Product import Product
 from Customer import Customer
+import uuid
 
 @dataclass
 class Order:
@@ -15,6 +16,7 @@ class Order:
     Attributs
         status : l'état de la commande "finished" ou "ongoing"
         customer : client (Costumer) qui a créé la commande
+        uid : identifiant unique de la commande
         date : la date à laquelle la commande a été réalisée, ici, par défaut la date courante
         line_orders : la liste des élements (Product) de la commande
     """
@@ -22,6 +24,7 @@ class Order:
 
     status: str
     customer: Customer
+    uid: str = field(default_factory=uuid.uuid4)
     date: datetime = field(default_factory=datetime.now)
     line_orders: List[Product] = field(default_factory=list)
 
@@ -36,6 +39,7 @@ class Order:
             print("Aucun produit enregistré")
         else:
             print(f"{f'{self.customer.firstname.title()} {self.customer.lastname.title()} voici votre commande du {self.date:%d/%m/%Y %H:%M}':^50}")
+            print(f"numéro de commande: {self.uid}")
             for line_order in self.line_orders:
                 print(f"- {line_order.name:<22} {line_order.stock:>3} {line_order.unit:<6} : {line_order.stock * line_order.price:>10.2f} €")
             print("- "*25)
