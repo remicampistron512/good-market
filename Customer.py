39ed439d850631974890b223a4ba0f36e4f0d455
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-22 14:25:55 +0200

    Ajout de docstrings pour la classe Customer

--- Customer.py
+++ Customer.py
@@ -1,25 +1,51 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+
+# classe Customer
+
 from dataclasses import dataclass
 from typing import ClassVar, List
 
 @dataclass
 class Customer:
-    firstName: str
-    lastName: str
+    """Classe représentant un client
+        – firstname : prénom du client
+        — lastname : nom du client
+    """
+    firstname: str
+    lastname: str
     customers: ClassVar[List['Customer']] = []
 
     def __init__(self, firstname: str, lastname: str):
-        self.firstName = firstname
-        self.lastName = lastname
+        """
+        Permet la création d'un client
+        :param firstname: Prenom du client
+        :param lastname: Nom du client
+        """
+        self.firstname = firstname
+        self.lastname = lastname
         Customer.customers.append(self)
 
     def __str__(self):
-        return f"{self.firstName} {self.lastName}"
+        """
+        Permet de configurer l'affichage d'un client
+        :return: Prenom et nom du client
+        """
+        return f"{self.firstname} {self.lastname}"
 
     def __repr__(self):
-        return f"Customer({self.firstName}, {self.lastName})"
+        """
+        Permet de configurer l'affichage d'un client pour le développement
+        :return: Prenom et nom du client
+        """
+        return f"Customer({self.firstname}, {self.lastname})"
 
     @classmethod
     def print_customers(cls):
+        """
+        Permet d'affichage de la liste des clients
+        :return: None
+        """
         if not cls.customers:
             print("Aucun client passé aujourd'hui")
         else:
