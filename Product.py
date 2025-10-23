637a35f9850dbc3f0677e1b6dafe16a170e1506f
Rémi Campistron <remi.campistron@gmail.com>
2025-10-23 20:09:50 +0200

    Suppression de print_product et add_product dans la classe Product

--- Product.py
+++ Product.py
@@ -9,12 +9,12 @@ class Product:
     """
     Classe représentant un produit
 
-    Attributs:
-        id: identifiant unique du produit
-        name: nom du produit
-        stock: quantité disponible
-        price: prix en euros
-        unit: unité de poids
+    Attributs :
+        id : identifiant unique du produit
+        name : nom du produit
+        stock : quantité disponible
+        price : prix en euros
+        unit : unité de poids
 
     """
     id: int
@@ -22,25 +22,3 @@ class Product:
     quantity: int
     price: float
     unit: str
-    products: ClassVar[List['Product']] = []
-
-
-    @classmethod
-    def print_products(cls):
-        """
-        Permet d'affichage de la liste des produits
-        """
-        if not cls.products:
-            print("Aucun produit enregistré")
-        else:
-            print("Voici la liste de produits :")
-            for product in cls.products:
-                print(f"- {product}")
-
-    @classmethod
-    def add_product(cls,product):
-        """
-        Ajoute un produit à la liste des produits
-        :param product: un objet product à ajouter
-        """
-        cls.products.append(product)
