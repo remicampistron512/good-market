773e83d5bd1a44c836f6a8f7dc4a1f9a3d2ae653
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-23 10:53:51 +0200

    Extraction de la méthode du choix 2 appellée review dans le fichier good_market

--- good_market.py
+++ good_market.py
@@ -7,6 +7,37 @@ from Order import Order
 from Inventory import Inventory
 
 
+def review(id_customer, id_product, orders):
+    Customer.create_customer(id_customer=id_customer, firstname="Pierre", lastname="Dupont")
+    Customer.create_customer(id_customer=id_customer + 1, firstname="Vanessa", lastname="Dubois")
+    apple = Product(id=id_product, name="Pomme", quantity=8, price=1.50, unit="kg")
+    pear = Product(id=id_product + 1, name="Poire", quantity=5, price=2.50, unit="kg")
+    Product.add_product(apple)
+    Product.add_product(pear)
+
+    order1 = Order(status="finished", customer=Customer.customers[0])
+    order1.add_line_order(apple)
+    order1.add_line_order(pear)
+    orders.append(order1)
+
+    order2 = Order(status="finished", customer=Customer.customers[1])
+    order2.add_line_order(apple)
+    orders.append(order2)
+
+    # --- Affichage global ---
+    print("\n=== CLIENTS ===")
+    Customer.print_customers()
+
+    print("\n=== Details des commandes des clients ===")
+    for order in orders:
+        order.print_order()
+        print("\n")
+
+    print("=== Stock Restant ===")
+    inventory = Inventory()
+    inventory.print_inventory()
+
+
 def main():
     id_customer = 1
     id_product = 1
@@ -35,38 +66,7 @@ def main():
                     inventory.update_stock(current_product)
 
         elif choice == '2':
-
-            Customer.create_customer(id_customer=id_customer, firstname="Pierre", lastname="Dupont")
-            Customer.create_customer(id_customer=id_customer + 1, firstname="Vanessa", lastname="Dubois")
-            Customer.create_customer(id_customer=id_customer + 2, firstname="Gustave", lastname="Fernand")
-            apple = Product(id=id_product, name="Pomme", quantity=8, price=1.50, unit="kg")
-            pear = Product(id=id_product + 1, name="Poire", quantity=5, price=2.50, unit="kg")
-            Product.add_product(apple)
-            Product.add_product(pear)
-
-            order1 = Order(status="finished", customer=Customer.customers[0])
-            order1.add_line_order(apple)
-            order1.add_line_order(pear)
-            orders.append(order1)
-
-            order2 = Order(status="finished", customer=Customer.customers[1])
-            order2.add_line_order(apple)
-            orders.append(order2)
-
-            # --- Affichage global ---
-            print("\n=== CLIENTS ===")
-            Customer.print_customers()
-
-            print("\n=== COMMANDES ===")
-            for order in orders:
-                order.print_order()
-                print("\n")
-
-            print("=== STOCK RESTANT ===")
-            inventory = Inventory()
-            inventory.print_inventory()
-
-            continue
+            review(id_customer, id_product, orders)
 
         elif choice.upper() == 'QUIT':
             print("Au revoir !")
